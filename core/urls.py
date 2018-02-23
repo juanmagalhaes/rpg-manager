a0a52c27a21f9fe2f76481ce5d84a1867a86cad0
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2018-02-23 20:14:49 -0300

    Flag disable django admin

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+import os
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^api/', include('rpg.urls')),
 ]
 
+enable_admin = os.environ.get('DJANGO_ADMIN', False) == 'True'
+
+if enable_admin:
+    urlpatterns.append(path('admin/', admin.site.urls))
