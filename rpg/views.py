fd82241d22e828af10b8c9d07e1d7f2ec2ad038c
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2018-02-23 20:09:09 -0300

    Remove unsed endpoints

--- rpg/views.py
+++ rpg/views.py
@@ -1,27 +1,15 @@
 from .serializers import (
-    AbilitySerializer,
     CharacterSerializer,
     GameSerializer,
-    ItemSerializer,
 )
 from .models import (
-    Ability,
     Character,
     Game,
-    Item,
 )
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
-
-class AbilityList(ListCreateAPIView):
-    queryset = Ability.objects.all()
-    serializer_class = AbilitySerializer
-
-class AbilityDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Ability.objects.all()
-    serializer_class = AbilitySerializer
 
 class CharacterList(ListCreateAPIView):
     queryset = Character.objects.all()
@@ -38,12 +26,4 @@ class GameList(ListCreateAPIView):
 class GameDetail(RetrieveUpdateDestroyAPIView):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
-
-class ItemList(ListCreateAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-
-class ItemDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
 
