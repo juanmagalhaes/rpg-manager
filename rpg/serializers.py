307d34f5ee275539252b4da45c84c03e52ea3c98
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2018-02-22 23:15:58 -0300

    Snapshot of these state to attempt new approach

--- rpg/serializers.py
+++ rpg/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
+from django.db import transaction
 from .models import (
     Ability,
     Character,
     Game,
     Item,
 )
+
+def map_to_related_model(character, Entity, data):
+    return list(map(lambda x:
+        Entity(character=character, **x), data))
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,27 +30,65 @@ class CharacterSerializer(serializers.ModelSerializer):
     items = ItemSerializer(many=True)
     abilities = AbilitySerializer(many=True)
 
+
+    @transaction.atomic
     def create(self, validated_data):
         items_data = validated_data.pop('items')
         abilities_data = validated_data.pop('abilities')
         character = Character.objects.create(**validated_data)
-
-        for item_data in items_data:
-            Item.objects.create(character=character, **item_data)
-
-        for ability_data in abilities_data:
-            Ability.objects.create(character=character, **ability_data)
-
+        character.items.bulk_create(map_to_related_model(
+            character,
+            Item,
+            items_data
+        ))
+        character.abilities.bulk_create(map_to_related_model(
+            character,
+            Ability,
+            abilities_data
+        ))
         return character
 
+    @transaction.atomic
     def update(self, instance, validated_data):
-        items_data = validated_data.pop('items')
-        abilities_data = validated_data.pop('abilities')
-
-        for k, v in validated_data.items():
-            setattr(instance, k, v if v else getattr(instance, k))
-
+        # XXX update all character properties
         instance.save()
+
+        import pdb; pdb.set_trace()
+
+        def create_new_nested_models(nested_set, collection):
+            new_nested_models = list(filter(lambda x: not x.id, collection))
+            nested_set.bulk_create(new_nested_models)
+
+        def update_existing_nested_models(nested_set, collection):
+            existing_nested_models = list(filter(lambda x: x.id, collection))
+            for item in existing_nested_models:
+                item.save()
+                nested_set.add(item)
+
+        def delete_old_nested_models(nested_set, collection):
+            old_nested_models = list(map(lambda x: x.id, collection))
+            nested_set.exclude(id__in=old_nested_models).delete()
+
+        items_data = validated_data.pop('items')
+        item_collection = map_to_related_model(
+            instance,
+            Item,
+            items_data
+        )
+        create_new_nested_models(instance.items, item_collection)
+        update_existing_nested_models(instance.items, item_collection)
+        delete_old_nested_models(instance.items, item_collection)
+
+        abilities = validated_data.pop('abilities')
+        ability_collection = map_to_related_model(
+            instance,
+            Ability,
+            abilities
+        )
+        create_new_nested_models(instance.abilities, ability_collection)
+        update_existing_nested_models(instance.abilities, ability_collection)
+        delete_old_nested_models(instance.abilities, ability_collection)
+
         return instance
 
     class Meta:
