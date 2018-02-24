7db7df94e49dc6f758a99baf90bf6ee50a0eac20
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2018-02-24 13:38:33 -0300

    Deploy attempt

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,5 +1,8 @@
  #!/bin/bash
 wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
