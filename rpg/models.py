bd10935e58220da1c0264399dc619bfd842a321e
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2018-02-28 13:07:27 -0300

    Fixing default date

--- rpg/models.py
+++ rpg/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.timezone import now
 
 class Game(models.Model):
     name = models.CharField(max_length=100)
     sumary = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=now, blank=True)
 
     def __str__(self):
         return self.name
